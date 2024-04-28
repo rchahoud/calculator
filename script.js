68190faf0319bf33717c3929ad70bfd9cfdfc660
roger chahoud <roger_chahoud@outlook.com>
2024-04-28 19:35:39 -0300

    Operate function and calculator variables

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(firstNumber, secondNumber, operator) {
+    return operator(firstNumber, secondNumber)
+}
+
+let firstNumber = 10;
+let secondNumber = 10;
+let operator = add;
+
+console.log(operate(firstNumber, secondNumber, operator));

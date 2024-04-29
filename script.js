269b05ebf807d666759d66ba7a3150398dadc16c
roger chahoud <roger_chahoud@outlook.com>
2024-04-28 21:39:33 -0300

    Basic HTML for calculator and event listener for each button

--- script.js
+++ script.js
@@ -15,11 +15,19 @@ function divide(a, b) {
 }
 
 function operate(firstNumber, secondNumber, operator) {
-    return operator(firstNumber, secondNumber);
+  return operator(firstNumber, secondNumber);
 }
 
 let firstNumber = 10;
 let secondNumber = 10;
 let operator = add;
+let display = '';
 
-console.log(operate(firstNumber, secondNumber, operator));
+let tableCells = document.querySelectorAll("button");
+
+tableCells.forEach(td => {
+    td.addEventListener('click', event => {
+        const buttonValue = event.target.value;
+        console.log(buttonValue);
+    })
+})

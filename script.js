79ad6954d95584c914901565ac1908cf17f486b9
roger chahoud <roger_chahoud@outlook.com>
2024-04-28 23:40:39 -0300

    Add, subtract, multiply and divide implementations

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const tableCells = document.querySelectorAll("button");
+
+let firstOperand = "";
+let lastOperand = "";
+let operator = null;
+let operandFlag = 0;
+let lastAction = "";
+
+function debug() {
+  console.log(
+    `Apos Operate(): firstOperand: ${firstOperand}, lastOperand:${lastOperand}, operandFlag: ${operandFlag}, operator: ${operator}`
+  );
+}
+
+function ac() {
+  firstOperand = "";
+  lastOperand = "";
+  operator = null;
+  operandFlag = 0;
+  display("");
+  debug();
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -14,20 +37,82 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(firstNumber, secondNumber, operator) {
-  return operator(firstNumber, secondNumber);
+function operate() {
+  const a = parseFloat(firstOperand);
+  const b = parseFloat(lastOperand);
+  firstOperand = operator(a, b);
+  display(firstOperand);
+  firstOperand = String(firstOperand);
+  //lastOperand = "";
+  console.log(`Resultado: ${firstOperand}`);
+  debug();
 }
 
-let firstNumber = 10;
-let secondNumber = 10;
-let operator = add;
-let display = '';
+function assignValue(value) {
+  if (operandFlag === 0) {
+    firstOperand = firstOperand + value;
+    console.log(`firstOperand: ${firstOperand} flag: ${operandFlag}`);
+  } else {
+    lastOperand = lastOperand + value;
+    console.log(`lastOperand: ${lastOperand} flag: ${operandFlag}`);
+  }
+}
 
-let tableCells = document.querySelectorAll("button");
+function display(value) {
+  const display = document.querySelector("#display");
+  display.textContent = value;
+}
 
-tableCells.forEach(td => {
-    td.addEventListener('click', event => {
-        const buttonValue = event.target.value;
-        console.log(buttonValue);
-    })
-})
+function clickHandle(input) {
+  switch (input) {
+    case "+":
+      if (operandFlag === 0) operandFlag = 1;
+      operator = add;
+      lastAction = "+";
+      break;
+    case "-":
+      if (operandFlag === 0) operandFlag = 1;
+      operator = subtract;
+      lastAction = "-";
+      break;
+    case "x":
+      if (operandFlag === 0) operandFlag = 1;
+      operator = multiply;
+      lastAction = "x";
+      break;
+    case "/":
+      if (operandFlag === 0) operandFlag = 1;
+      operator = divide;
+      lastAction = "/";
+      break;
+    case "%":
+      lastAction = "%";
+      break;
+    case "+/-":
+      lastAction = "+/-";
+      break;
+    case "AC":
+      ac();
+      lastAction = "AC";
+      break;
+    case ".":
+      lastAction = ".";
+      break;
+    case "=":
+      operate();
+      lastAction = "=";
+      break;
+    default:
+      if (lastAction === "=") ac();
+      assignValue(input);
+      lastAction = "default";
+      break;
+  }
+}
+
+tableCells.forEach((td) => {
+  td.addEventListener("click", (event) => {
+    const buttonValue = event.target.value;
+    clickHandle(buttonValue);
+  });
+});
